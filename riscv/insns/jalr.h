2390bfb043707272dda2acf85ed6c4dcc56b5179
saitenntaisei <saitenntaisei@gmail.com>
2025-08-26 16:57:26 +0900

    feat: update CHECK_RA macro to use RS1 and restore its invocation in jalr instruction

--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,7 +1,7 @@
 reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
 if (insn.rd() == 0 && insn.i_imm() == 0) {
-    // CHECK_RA(RS1);
-    std::cerr << "[LOG] check_ra called at "<< " "<<insn.rd()<< std::endl;
+    CHECK_RA(RS1);
+    
 }
 WRITE_RD(tmp);
--- riscv/processor.cc
+++ riscv/processor.cc
@@ -385,7 +385,7 @@ void state_t::write_xpr(size_t i, reg_t value) {
 }
 
 void state_t::check_ra(size_t i, reg_t value) {
-  // std::cerr << "[LOG] check_ra called at " << __FILE__ << ":" << __LINE__ << std::endl;
+  std::cerr << "[LOG] check_ra called at " << __FILE__ << ":" << __LINE__ <<" "<<i<< std::endl;
   reg_t shadow_value = shadow_stack_pop();
   if (shadow_value != value) {
     fprintf(stderr, "error: incorrect stack\n");
