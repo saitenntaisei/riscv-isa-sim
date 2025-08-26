d4ff99cae8808ece4fdd25089069632b34b99dda
saitenntaisei <saitenntaisei@gmail.com>
2025-08-26 16:55:00 +0900

    feat: comment out CHECK_RA calls and logging in jalr and processor

--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,6 +1,7 @@
 reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
-if (insn.rs1() == X_RA && insn.rd() == 0 && insn.i_imm() == 0) {
-    CHECK_RA(RS1);
+if (insn.rd() == 0 && insn.i_imm() == 0) {
+    // CHECK_RA(RS1);
+    std::cerr << "[LOG] check_ra called at "<< " "<<insn.rd()<< std::endl;
 }
 WRITE_RD(tmp);
--- riscv/processor.cc
+++ riscv/processor.cc
@@ -379,13 +379,13 @@ void state_t::write_xpr(size_t i, reg_t value) {
  
   XPR.write(i, value);
   if (i == X_RA) {
-    std::cerr << "[LOG] write_xpr called at " << __FILE__ << ":" << __LINE__ << std::endl;
+    // std::cerr << "[LOG] write_xpr called at " << __FILE__ << ":" << __LINE__ << std::endl;
     shadow_stack_push(value);
   }
 }
 
 void state_t::check_ra(size_t i, reg_t value) {
-  std::cerr << "[LOG] check_ra called at " << __FILE__ << ":" << __LINE__ << std::endl;
+  // std::cerr << "[LOG] check_ra called at " << __FILE__ << ":" << __LINE__ << std::endl;
   reg_t shadow_value = shadow_stack_pop();
   if (shadow_value != value) {
     fprintf(stderr, "error: incorrect stack\n");
