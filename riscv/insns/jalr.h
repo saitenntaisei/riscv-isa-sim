8cded3dce0fa07c6c16387cde8430d6213e97263
saitenntaisei <saitenntaisei@gmail.com>
2025-08-26 16:33:16 +0900

    feat: use easy condition

--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,6 +1,6 @@
 reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
-if (insn.rs1() == X_RA && insn.rd() == 0 && insn.i_imm() == 0) { 
+if (insn.rs1() == X_RA) { 
     CHECK_RA(RS1);
 }
 WRITE_RD(tmp);
