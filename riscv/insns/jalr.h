e2ccd4867430cb8a192930d6e3cef1a10d0f34b0
saitenntaisei <saitenntaisei@gmail.com>
2025-08-26 16:43:15 +0900

    feat: test

--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,6 +1,6 @@
 reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
- 
-CHECK_RA(RS1);
-
+if (insn.rd() == 0 && insn.i_imm() == 0) { 
+    CHECK_RA(RS1);
+}
 WRITE_RD(tmp);
--- riscv/processor.cc
+++ riscv/processor.cc
@@ -386,10 +386,6 @@ void state_t::write_xpr(size_t i, reg_t value) {
 
 void state_t::check_ra(size_t i, reg_t value) {
   std::cerr << "[LOG] check_ra called at " << __FILE__ << ":" << __LINE__ << std::endl;
-  if (i != X_RA) {
-    fprintf(stderr, "error: should receive ra but got %zu\n", i);
-    std::abort();
-  }
   reg_t shadow_value = shadow_stack_pop();
   if (shadow_value != value) {
     fprintf(stderr, "error: incorrect stack\n");
