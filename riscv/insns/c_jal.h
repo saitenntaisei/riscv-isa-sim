802da5a1d7ff050ac16524d2969f30e213743f7e
saitenntaisei <saitenntaisei@gmail.com>
2025-08-27 12:41:36 +0900

    feat: add RECORD_RA call in c_jal for return address handling

--- riscv/insns/c_jal.h
+++ riscv/insns/c_jal.h
@@ -2,6 +2,7 @@ require_extension('C');
 if (xlen == 32) {
   reg_t tmp = npc;
   set_pc(pc + insn.rvc_j_imm());
+  RECORD_RA(X_RA);
   WRITE_REG(X_RA, tmp);
 } else { // c.addiw
   require(insn.rvc_rd() != 0);
